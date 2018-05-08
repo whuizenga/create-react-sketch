0be6ddcf4235a21833615a2e73c98dd1118f30b3
whuizenga <will.huizenga@gmail.com>
2018-05-08 16:44:20 -0400

    finished bootstrap component with styled components

--- templates/default/src/main.js
+++ templates/default/src/main.js
@@ -1,10 +1,48 @@
+/* eslint-disable react/prop-types */
 import React, { Component } from 'react'
-import { Artboard, Text, View, render } from 'react-sketchapp'
+import { render, View, Text, Image } from 'react-sketchapp'
+import styled from 'styled-components/primitives'
+
+const Header = styled.View`
+    width: 500px;
+    height: 190px;
+    background-color: #24292F;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+`;
+
+const Title = styled.Text`
+    font-size: 24px;
+    margin-bottom: 16px;
+    color: #FFAD00;
+`
+
+const Body = styled.View`
+    display: flex;
+    align-items: center;
+    padding: 8px;
+`
+
+const Code = styled.Text`
+    font-family: Courier;
+`
+const Logo = styled.Image`
+    height: 125px;
+    width: 125px;
+    margin-top: 16px;
+`
 
 const Document = (props) => {
     return (
         <View>
-            <Text>Hello World!</Text>
+            <Header>
+                <Logo source='https://i.imgur.com/NtLLscc.png' />
+                <Title>Welcome to React-sketchapp!</Title>
+            </Header>
+            <Body>
+                <Text>To get started, edit <Code>src/main.js</Code> and save to reload.</Text>
+            </Body>
         </View>
     )
 }
